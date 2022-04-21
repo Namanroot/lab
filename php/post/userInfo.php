b58869b96d5147d9c2dae7af2eeba248feb76140
Namanroot <104104288+Namanroot@users.noreply.github.com>
2022-04-21 08:18:06 +0530

    Add files via upload

---
+++ php/post/userInfo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome.php" method="post">
+        Name : <input type="text" name="name" ><br>
+        Email : <input type="text" name="email" ><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ php/post/welcome.php
@@ -0,0 +1,7 @@
+<?php 
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    echo "Welcome $name \n";
+    echo "Email $email ";
+ ?>
